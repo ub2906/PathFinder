91c32ec13bcd71562fe4fdbc2ebb67023a3f2605
mehakmalhotra1 <50541441+mehakmalhotra1@users.noreply.github.com>
2020-07-24 00:30:41 +0530

    Update getNeighbors.js

--- public/browser/getNeighbors.js
+++ public/browser/getNeighbors.js
@@ -1,11 +1,11 @@
-const unweightedSearchAlgorithm = require("./pathfindingAlgorithms/unweightedSearchAlgorithm");
+const DFSnBFS = require("./pathfindingAlgorithms/DFSnBFS");
 function getNeighbors(id, nodes, boardArray,name) {
     let coordinates = id.split("-");
     let x = parseInt(coordinates[0]);
     let y = parseInt(coordinates[1]);
     let neighbors = [];
     let potentialNeighbor;
-    if(name === unweightedSearchAlgorithm){
+    if(name === DFSnBFS){
       if (boardArray[x - 1] && boardArray[x - 1][y]) {
         potentialNeighbor = `${(x - 1).toString()}-${y.toString()}`
         if (nodes[potentialNeighbor].status !== "wall") {
