99d6abb0b6783bea16d53495ed81860aad7eb1f6
Urja Banati <48362988+ub2906@users.noreply.github.com>
2020-07-24 18:06:47 +0530

    Update panel.js

--- public/browser/panel.js
+++ public/browser/panel.js
@@ -1,45 +1,42 @@
+//to make the instruction box movable 
 dragElement(document.getElementById("instructions_panel"));
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   if (document.getElementById(elmnt.id + "header")) {
-    // if present, the header is where you move the DIV from:
     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
   } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
     elmnt.onmousedown = dragMouseDown;
   }
 
   function dragMouseDown(e) {
     e = e || window.event;
     e.preventDefault();
-    // get the mouse cursor position at startup:
     pos3 = e.clientX;
     pos4 = e.clientY;
     document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
   }
 
   function elementDrag(e) {
     e = e || window.event;
     e.preventDefault();
-    // calculate the new cursor position:
+    //posn of mouse cursor
     pos1 = pos3 - e.clientX;
     pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
     pos4 = e.clientY;
-    // set the element's new position:
+    // setting new posn
     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
   }
 
   function closeDragElement() {
-    // stop moving when mouse button is released:
+    // stops when draggind is stopped
     document.onmouseup = null;
     document.onmousemove = null;
   }
-  
+//Hide button on top right corner to close panel  
   $('#hide_instructions').click(function() {
     $('#instructions_panel').slideUp();
 });
